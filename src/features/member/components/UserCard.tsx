4fa1b7952a3fafcfe6072a4e6ef8ec7a6249fb7c
leeeydia <leeeydia@gmail.com>
2026-02-27 14:17:37 +0900

    feat: 사용자 프로필 페이지에서 MyBoardList 컴포넌트 추가 및 불필요한 코드 제거

--- src/features/member/components/UserCard.tsx
+++ src/features/member/components/UserCard.tsx
@@ -1,10 +1,5 @@
-"use client";
-
 import { useEffect, useState } from "react";
 import axiosInstance from "../../../shared/api/axiosInstance";
-
-type ReplyMode = "TEACHER" | "PARENT";
-
 interface MemberResponse {
   id: number;
   username: string;
