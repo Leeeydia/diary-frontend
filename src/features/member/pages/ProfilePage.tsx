7ee43ae3c6e93b94fa1f9e76d0f0182a850475b4
leeeydia <leeeydia@gmail.com>
2026-02-27 15:27:07 +0900

    feat: 프로필 페이지에 Header 컴포넌트 추가

--- src/features/member/pages/ProfilePage.tsx
+++ src/features/member/pages/ProfilePage.tsx
@@ -1,12 +1,17 @@
+import Header from '../../../shared/components/Header';
 import UserCard from '../components/UserCard';
 import MyBoardList from '../components/MyBoardList';
 
 function ProfilePage() {
   return (
-    <div className="max-w-2xl mx-auto p-8 flex flex-col gap-8">
-      <UserCard />
-      <MyBoardList />
-    </div>
+    <>
+      <Header />
+      <div className="max-w-2xl mx-auto p-8 flex flex-col gap-8">
+        <UserCard />
+        <MyBoardList />
+      </div>
+    </>
   );
 }
+
 export default ProfilePage;
